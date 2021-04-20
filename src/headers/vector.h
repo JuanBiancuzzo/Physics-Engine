48a37e4d9a5678c1df281943c11102ddef4c999a
JuanBiancuzzo <jbiancuzzo@fi.uba.ar>
2021-04-20 14:08:17 -0300

    Creando el paquete vector2
    Principalmente tiene modificaciones o agregando las implementaciones de los operadores para el uso de vectores en dos dimensiones

---
+++ src/headers/vector.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <iostream>
+
+struct Vector2
+{
+    float x, y;
+
+    Vector2();
+
+    Vector2(float x, float y);
+
+    Vector2 operator+(const Vector2 &otro) const;
+
+    Vector2 operator-(const Vector2 &otro) const;
+
+    Vector2 operator*(float alpha) const;
+
+    Vector2 operator/(float alpha) const;
+
+    float operator*(const Vector2 &otro) const; // producto interno
+
+    void operator+=(const Vector2 &otro);
+
+    void operator-=(const Vector2 &otro);
+
+    float modulo() const;
+
+    float distancia(const Vector2 &otro) const;
+
+    void imprimir() const;
+};
---
+++ src/vector.cpp
@@ -0,0 +1,59 @@
+#include "headers/vector.h"
+
+#include <math.h>
+
+Vector2::Vector2() : x(.0f), y(.0f) {}
+
+Vector2::Vector2(float x, float y) : x(x), y(y) {}
+
+Vector2 Vector2::operator+(const Vector2 &otro) const
+{
+    return Vector2(x + otro.x, y + otro.y);
+}
+
+Vector2 Vector2::operator-(const Vector2 &otro) const
+{
+    return Vector2(x - otro.x, y - otro.y);
+}
+
+Vector2 Vector2::operator*(float alpha) const
+{
+    return Vector2(x * alpha, y * alpha);
+}
+
+Vector2 Vector2::operator/(float alpha) const
+{
+    return Vector2(x / alpha, y / alpha);
+}
+
+float Vector2::operator*(const Vector2 &otro) const
+{
+    return (x * otro.x + y * otro.y);
+}
+
+void Vector2::operator+=(const Vector2 &otro)
+{
+    x += otro.x;
+    y += otro.y;
+}
+
+void Vector2::operator-=(const Vector2 &otro)
+{
+    x -= otro.x;
+    y -= otro.y;
+}
+
+float Vector2::modulo() const
+{
+    return sqrt(pow(x, 2) + pow(y, 2));
+}
+
+float Vector2::distancia(const Vector2 &otro) const 
+{
+    return (*this - otro).modulo();
+}
+
+void Vector2::imprimir() const 
+{
+    std::cout << x << ", " << y << std::endl;
+}
