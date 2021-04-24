94fb76e148af30ce2239b06941c8c5bcc56cf649
JuanBiancuzzo <jbiancuzzo@fi.uba.ar>
2021-04-24 11:24:47 -0300

    Implementando el quadtree
    Todavia no probe si funcion el actualizar, pero insertar, buscar y eliminar funcionan bien

---
+++ src/node.cpp
@@ -0,0 +1,131 @@
+
+#include "headers/node.h"
+
+Node::Node(const Rectangulo &nueva_area)
+    : c_area(nueva_area), c_dividido(false), c_cant_particulas(0)
+{
+}
+
+Node::~Node()
+{
+    if (c_dividido)
+        for (Node *node : c_subdivisiones)
+            delete node;
+}
+
+Node *Node::buscar_hoja(const Particula &particula)
+{
+    if (c_area.contiene(particula))
+    {
+        if (c_dividido)
+        {
+            int index = calcular_index(particula);
+            return (c_subdivisiones[index])->buscar_hoja(particula);
+        }
+        return this;
+    }
+    return nullptr;
+}
+
+int Node::calcular_index(const Particula &particula) const
+{
+    Vector2 delta = particula.c_pos - c_area.c_pos;
+
+    int index_x = (!delta.x) ? 0 : static_cast<int>((1 + delta.x / abs(delta.x)) / 2);
+    int index_y = (!delta.y) ? 0 : static_cast<int>((1 - delta.y / abs(delta.y)) / 2);
+
+    return (index_x + 2 * index_y);
+}
+
+/* _________
+ * |_0_|_1_| -> esta es la forma en la que se van a organizar
+ * |_2_|_3_|
+ */
+void Node::subdividir()
+{
+    float nuevo_w = c_area.c_w / 2;
+    float nuevo_h = c_area.c_h / 2;
+
+    for (int i = 0; i < capacidad_sub / 2; i++)
+    {
+        for (int j = 0; j < capacidad_sub / 2; j++)
+        {
+            float nuevo_x = c_area.c_pos.x + nuevo_w * (2 * j - 1);
+            float nuevo_y = c_area.c_pos.y + nuevo_h * (1 - 2 * i);
+
+            int index = j + 2 * i;
+            c_subdivisiones[index] = new Node(Rectangulo(Vector2(nuevo_x, nuevo_y), nuevo_w, nuevo_h));
+        }
+    }
+    for (int i = 0; i < c_cant_particulas; i++)
+    {
+        int index = this->calcular_index(*(c_particulas[i]));
+        this->c_subdivisiones[index]->insertar(c_particulas[i]);
+    }
+    c_dividido = true;
+}
+
+void Node::insertar(Particula *particula)
+{
+
+    int index = calcular_index(*particula);
+    if (c_cant_particulas >= capacidad_particulas)
+    {
+        if (!c_dividido)
+            subdividir();
+        c_subdivisiones[index]->insertar(particula);
+        c_cant_particulas++;
+    }
+    else
+    {
+        particula->c_index = c_cant_particulas;
+        particula->c_padre = this;
+        c_particulas[c_cant_particulas] = particula;
+        c_cant_particulas++;
+    }
+}
+
+void Node::actualizar(Particula &particula, Node *nuevo_nodo)
+{
+    eliminar(particula);
+    nuevo_nodo->insertar(&particula);
+}
+
+Particula *Node::eliminar(Particula &particula)
+{
+    Node *subdivision = (Node *)(particula.c_padre);
+
+    int index = particula.c_index;
+    Particula *aux = subdivision->c_particulas[index];
+
+    int ultimo_index = subdivision->c_cant_particulas - 1;
+    Particula *ultima_particula = subdivision->c_particulas[ultimo_index];
+    subdivision->c_particulas[index] = ultima_particula;
+    ultima_particula->c_index = index;
+    c_cant_particulas--;
+
+    return aux;
+}
+
+void Node::buscar(const Rectangulo &frontera, Particula *output[], int &cantidad)
+{
+    if (!c_area.intersecta(frontera))
+        return;
+
+    if (c_dividido)
+    {
+        for (int i = 0; i < capacidad_sub; i++)
+            c_subdivisiones[i]->buscar(frontera, output, cantidad);
+    }
+    else
+    {
+        for (int i = 0; i < c_cant_particulas; i++)
+        {
+            if (frontera.contiene(*c_particulas[i]))
+            {
+                output[cantidad] = c_particulas[i];
+                cantidad++;
+            }
+        }
+    }
+}
---
+++ src/quadtree.cpp
@@ -0,0 +1,41 @@
+#include "headers/quadtree.h"
+
+QuadTree::QuadTree(const Rectangulo &nueva_area)
+    : c_area(nueva_area)
+{
+    c_raiz = new Node(nueva_area);
+}
+
+QuadTree::~QuadTree()
+{
+    delete c_raiz;
+}
+
+bool QuadTree::insertar(Particula *particula) 
+{
+    if (!c_area.contiene(*particula))
+        return false;
+    c_raiz->insertar(particula);
+    return true;
+}
+
+void QuadTree::actualizar(Particula &particula)
+{
+    Node *hoja_nueva = c_raiz->buscar_hoja(particula);
+
+    if (hoja_nueva != particula.c_padre)
+        c_raiz->actualizar(particula, hoja_nueva);
+}
+
+Particula* QuadTree::eliminar(Particula &particula)
+{
+    if (!c_area.contiene(particula))
+        return nullptr;
+    return c_raiz->eliminar(particula);
+}
+
+void QuadTree::buscar(const Rectangulo &frontera, Particula *output[], int &cantidad)
+{
+    if (c_area.intersecta(frontera)) 
+        c_raiz->buscar(frontera, output, cantidad);
+}
