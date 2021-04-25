021b2b9012070b96f0131cbba2e584d2205b9f82
JuanBiancuzzo <jbiancuzzo@fi.uba.ar>
2021-04-24 22:17:30 -0300

    Update rectangulo.cpp
    No es necesario que sea <= o >= porque si estan exactamente los bordes implica que no puede haber particulas ahi

--- src/rectangulo.cpp
+++ src/rectangulo.cpp
@@ -14,17 +14,17 @@ Rectangulo::Rectangulo(const Vector2 pos, float w, float h)
 bool Rectangulo::contiene(const Entidad &entidad) const
 {
     return !(
-        c_pos.x + c_w <= entidad.c_pos.x ||
-        c_pos.x - c_w >= entidad.c_pos.x ||
-        c_pos.y + c_h <= entidad.c_pos.y ||
-        c_pos.y - c_h >= entidad.c_pos.y);
+        c_pos.x + c_w < entidad.c_pos.x ||
+        c_pos.x - c_w > entidad.c_pos.x ||
+        c_pos.y + c_h < entidad.c_pos.y ||
+        c_pos.y - c_h > entidad.c_pos.y);
 }
 
 bool Rectangulo::intersecta(const Rectangulo &frontera) const
 {
     return !(
-        c_pos.x + c_w <= frontera.c_pos.x - frontera.c_w ||
-        c_pos.x - c_w >= frontera.c_pos.x + frontera.c_w ||
-        c_pos.y + c_h <= frontera.c_pos.y - frontera.c_h ||
-        c_pos.y - c_h >= frontera.c_pos.y + frontera.c_h);
+        c_pos.x + c_w < frontera.c_pos.x - frontera.c_w ||
+        c_pos.x - c_w > frontera.c_pos.x + frontera.c_w ||
+        c_pos.y + c_h < frontera.c_pos.y - frontera.c_h ||
+        c_pos.y - c_h > frontera.c_pos.y + frontera.c_h);
 }
