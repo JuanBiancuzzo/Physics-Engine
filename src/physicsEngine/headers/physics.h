85a810e04496bbb5eec54c7a22ffb5211ad60027
JuanBiancuzzo <jbiancuzzo@fi.uba.ar>
2021-04-26 18:57:11 -0300

    Creando las bases del motor fisica
    Agregando la interfaz del usuario

--- src/physicsEngine/headers/physics.h
+++ src/physicsEngine/headers/physics.h
@@ -1,6 +1,25 @@
 #pragma once
 
+#include <vector>
+
 #include "quadtree.h"
 #include "particula.h"
 #include "rectangulo.h"
 #include "vector.h"
+
+class Fisicas
+{
+private:
+    QuadTree c_qt;
+    std::vector<Particula *> c_particulas;
+
+public:
+    Fisicas(const Rectangulo &mundo);
+    ~Fisicas();
+
+    void insertar_particula(const Particula &particula);
+    void insertar_particula(const Vector2 &pos);
+
+    void mostrar() const;
+    void avanzar(const float delta_tiempo);
+};
--- src/physicsEngine/physics.cpp
+++ src/physicsEngine/physics.cpp
@@ -1,3 +1,49 @@
-
-
 #include "headers/physics.h"
+
+Fisicas::Fisicas(const Rectangulo &mundo)
+    : c_qt(mundo)
+{
+}
+
+Fisicas::~Fisicas()
+{
+    for (Particula *particula : c_particulas)
+        delete particula;
+}
+
+void Fisicas::insertar_particula(const Particula &particula)
+{
+    Particula *p = new Particula(particula.c_pos, particula.c_masa, particula.c_radio);
+    c_qt.insertar(p);
+    c_particulas.emplace_back(p);
+}
+
+void Fisicas::insertar_particula(const Vector2 &pos)
+{
+    insertar_particula(Particula(pos));
+}
+
+void Fisicas::mostrar() const
+{
+    // cuando tenga una forma de renderizas aca deberia estar
+}
+
+void Fisicas::avanzar(const float delta_tiempo)
+{
+    for (Particula *particula : c_particulas)
+    {
+        // calcular fuerzas
+        /*
+             * rozamiento
+             * interacciones con otros objetos
+             * futuras otras fuerzas
+             */
+        // particula->aplicar_fuerza();
+    }
+
+    for (Particula *particula : c_particulas)
+    {
+        particula->actualizar(delta_tiempo);
+        c_qt.actualizar(*particula);
+    }
+}
