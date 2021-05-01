a679c6c13bd6c6604b3282386a45f13e5a545c19
JuanBiancuzzo <jbiancuzzo@fi.uba.ar>
2021-05-01 10:07:27 -0300

    Moviendo nodo.h a quadtree.h, y nodo.cpp a quadtree.cpp
    Me parece que tiene que estar juntos, teniendo en cuenta como dependen el uno del otro
    Al ser solo dos archivos los saque de la carpeta quadtree, y ahora simplemente estan en la carpeta de herramientas

--- src/herramientas/quadtree.cpp
+++ src/herramientas/quadtree.cpp
@@ -1,5 +1,57 @@
+#include "quadtree.h"
 
-#include "node.h"
+QuadTree::QuadTree(const Rectangulo &nueva_area)
+    : c_area(nueva_area)
+{
+    c_raiz = new Node(nueva_area);
+}
+
+QuadTree::~QuadTree()
+{
+    delete c_raiz;
+}
+
+bool QuadTree::insertar(Entidad *entidad)
+{
+    if (!c_area.contiene(*entidad))
+        return false;
+    c_raiz->insertar(entidad);
+    return true;
+}
+
+bool QuadTree::actualizar(Entidad *entidad)
+{
+    if (!c_area.contiene(*entidad))
+        return false;
+    Node *hoja_nueva = c_raiz->buscar_hoja(entidad);
+
+    if (hoja_nueva != entidad->c_padre)
+        c_raiz->actualizar(entidad);
+    return true;
+}
+
+void QuadTree::eliminar(Entidad *entidad)
+{
+    if (c_area.contiene(*entidad))
+        c_raiz->eliminar(entidad);
+}
+
+void QuadTree::buscar(const Area *frontera, Entidad *output[], int &cantidad)
+{
+    cantidad = 0;
+    if (c_area.intersecta(*frontera))
+        c_raiz->buscar(frontera, output, cantidad);
+}
+
+int QuadTree::cantidad(const Area *frontera)
+{
+    int cantidad = 0;
+    if (c_area.intersecta(*frontera))
+        c_raiz->cantidad(frontera, cantidad);
+    return cantidad;
+}
+
+/* ----- Nodo ----- */
 
 Node::Node(const Rectangulo &nueva_area)
     : c_area(nueva_area), c_cant_entidades(0), c_dividido(false)
@@ -13,7 +65,7 @@ Node::~Node()
             delete node;
 }
 
-Node *Node::buscar_hoja(const Entidad &entidad)
+Node *Node::buscar_hoja(const Entidad *entidad)
 {
     if (c_dividido)
     {
@@ -27,9 +79,9 @@ Node *Node::buscar_hoja(const Entidad &entidad)
  * |_0_|_1_| -> esta es la forma en la que se van a organizar
  * |_2_|_3_|
  */
-int Node::calcular_index(const Entidad &entidad) const
+int Node::calcular_index(const Entidad *entidad) const
 {
-    Vector2 delta = entidad.c_pos - c_area.c_pos;
+    Vector2 delta = entidad->c_pos - c_area.c_pos;
 
     int index_x = (delta.x == 0) ? 0 : static_cast<int>((1 + delta.x / abs((int)delta.x)) / 2);
     int index_y = (delta.y == 0) ? 0 : static_cast<int>((1 - delta.y / abs((int)delta.y)) / 2);
@@ -55,7 +107,7 @@ void Node::subdividir()
     }
     for (int i = 0; i < c_cant_entidades; i++)
     {
-        int index = calcular_index(*(c_entidades[i]));
+        int index = calcular_index(c_entidades[i]);
         c_subdivisiones[index]->insertar(c_entidades[i]);
     }
     c_dividido = true;
@@ -65,7 +117,7 @@ void Node::insertar(Entidad *entidad)
 {
     if (c_cant_entidades >= capacidad_entidades)
     {
-        int index = calcular_index(*entidad);
+        int index = calcular_index(entidad);
         if (!c_dividido)
             subdividir();
         c_subdivisiones[index]->insertar(entidad);
@@ -79,12 +131,12 @@ void Node::insertar(Entidad *entidad)
     c_cant_entidades++;
 }
 
-void Node::actualizar(Entidad &entidad)
+void Node::actualizar(Entidad *entidad)
 {
-    Entidad entidad_vieja = entidad;
-    entidad_vieja.c_pos = ((Node *)entidad.c_padre)->c_area.c_pos;
-    eliminar(entidad_vieja);
-    insertar(&entidad);
+    Entidad entidad_vieja = *entidad;
+    entidad_vieja.c_pos = ((Node *)entidad->c_padre)->c_area.c_pos;
+    eliminar(&entidad_vieja);
+    insertar(entidad);
 }
 
 void Node::juntar()
@@ -92,7 +144,7 @@ void Node::juntar()
     if (c_cant_entidades >= capacidad_entidades)
         return;
     int cantidad = 0;
-    buscar(c_area, c_entidades, cantidad);
+    buscar(&c_area, c_entidades, cantidad);
 
     for (int i = 0; i < cantidad; i++)
     {
@@ -105,29 +157,25 @@ void Node::juntar()
         node->~Node();
 }
 
-Entidad *Node::eliminar(Entidad &entidad)
+void Node::eliminar(Entidad *entidad)
 {
-    Entidad *eliminar;
-    if (!c_dividido)
-    {
-        eliminar = c_entidades[entidad.c_index];
-        c_entidades[entidad.c_index] = c_entidades[c_cant_entidades - 1];
-        c_cant_entidades--;
-    }
-    else
+    if (c_dividido)
     {
         int index = calcular_index(entidad);
         c_cant_entidades--;
-        eliminar = c_subdivisiones[index]->eliminar(entidad);
+        c_subdivisiones[index]->eliminar(entidad);
         if (c_cant_entidades < capacidad_entidades)
             juntar();
+        return;
     }
-    return eliminar;
+
+    c_entidades[entidad->c_index] = c_entidades[c_cant_entidades - 1];
+    c_cant_entidades--;
 }
 
-void Node::buscar(const Rectangulo &frontera, Entidad *output[], int &cantidad)
+void Node::buscar(const Area *frontera, Entidad *output[], int &cantidad)
 {
-    if (!c_area.intersecta(frontera))
+    if (!c_area.intersecta(*frontera))
         return;
 
     if (c_dividido)
@@ -139,7 +187,7 @@ void Node::buscar(const Rectangulo &frontera, Entidad *output[], int &cantidad)
 
     for (int i = 0; i < c_cant_entidades; i++)
     {
-        if (frontera.contiene(*c_entidades[i]))
+        if (frontera->contiene(*c_entidades[i]))
         {
             output[cantidad] = c_entidades[i];
             cantidad++;
@@ -147,9 +195,9 @@ void Node::buscar(const Rectangulo &frontera, Entidad *output[], int &cantidad)
     }
 }
 
-void Node::cantidad(const Rectangulo &frontera, int &cantidad)
+void Node::cantidad(const Area *frontera, int &cantidad)
 {
-    if (!c_area.intersecta(frontera))
+    if (!c_area.intersecta(*frontera))
         return;
 
     if (c_dividido)
@@ -160,6 +208,6 @@ void Node::cantidad(const Rectangulo &frontera, int &cantidad)
     }
 
     for (int i = 0; i < c_cant_entidades; i++)
-        if (frontera.contiene(*c_entidades[i]))
+        if (frontera->contiene(*c_entidades[i]))
             cantidad++;
 }
---
+++ src/herramientas/quadtree.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "area.h"
+#include "entidad.h"
+
+class Node;
+
+class QuadTree
+{
+public:
+    Rectangulo c_area;
+
+private:
+    Node *c_raiz;
+
+public:
+    QuadTree(const Rectangulo &nueva_area);
+    ~QuadTree();
+
+    bool insertar(Entidad *entidad);
+    bool actualizar(Entidad *entidad);
+    void eliminar(Entidad *entidad);
+    void buscar(const Area *frontera, Entidad *output[], int &cantidad);
+    int cantidad(const Area *frontera);
+};
+
+class Node
+{
+public:
+    Rectangulo c_area;
+
+private:
+    static const int capacidad_entidades = 4;
+    static const int capacidad_sub = 4;
+
+    Entidad *c_entidades[capacidad_entidades];
+    int c_cant_entidades;
+
+    Node *c_subdivisiones[capacidad_sub];
+    bool c_dividido;
+
+public:
+    Node(const Rectangulo &nueva_area);
+    ~Node();
+
+    void insertar(Entidad *entidad);
+    void actualizar(Entidad *entidad);
+    void eliminar(Entidad *entidad);
+    void buscar(const Area *frontera, Entidad *output[], int &cantidad);
+    void cantidad(const Area *frontera, int &cantidad);
+
+    Node *buscar_hoja(const Entidad *entidad);
+
+private:
+    int calcular_index(const Entidad *entidad) const;
+    void subdividir();
+    void juntar();
+};
