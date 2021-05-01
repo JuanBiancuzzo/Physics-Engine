981e3b18801251e079bae0ee3e97fbd3ed8245e3
JuanBiancuzzo <jbiancuzzo@fi.uba.ar>
2021-05-01 10:16:54 -0300

    Limpeando el main
    Todo lo que tenia en el main era para poder probar el quadtree cuando lo estaba haciendo, y para eso ya tengo las pruebas, entonces hay menos motivos por el que el main tenga que tener todo eso

--- main/main.cpp
+++ main/main.cpp
@@ -1,27 +1,9 @@
 #include <iostream>
 
-#include "../src/physics_engine/headers/physics.h"
+#include "../src/physics_engine/physics.h"
 
 int main()
 {
-
-    // QuadTree qt(Rectangulo(Vector2(.0f, .0f), 64.0f, 64.0f));
-    // const int cantidad = 5;
-    // Particula *particulas[cantidad];
-    // particulas[0] = new Particula(Vector2(30.0f, 30.0f));
-    // particulas[1] = new Particula(Vector2(10.0f, 10.0f));
-    // particulas[2] = new Particula(Vector2(30.0f, 10.0f));
-    // particulas[3] = new Particula(Vector2(10.0f, 30.0f));
-    // particulas[4] = new Particula(Vector2(20.0f, 20.0f));
-
-    // for (int i = 0; i < cantidad; i++)
-    //     qt.insertar(particulas[i]);
-
-    // particulas[4]->c_pos = Vector2(-30.0f, -30.0f);
-    // qt.actualizar(*particulas[4]);
-
-    // for (int i = 0; i < cantidad; i++)
-    //     delete particulas[i];
 
     return 0;
 }
