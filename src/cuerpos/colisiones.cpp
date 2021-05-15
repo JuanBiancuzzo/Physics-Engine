a1f7c914d31ae6ead5b4502d70fca4f9b867673d
JuanBiancuzzo <jbiancuzzo@fi.uba.ar>
2021-05-15 08:12:07 -0300

    Me habia olvidado de incluir el cuerpo rigido, y el valor de retorno de las funciones, tambien agregue el caso linea linea aunque todavia no lo hice

--- src/cuerpos/colisiones.cpp
+++ src/cuerpos/colisiones.cpp
@@ -1,20 +1,28 @@
+#include "cuerpoRigido.h"
+
 #include "ciruclo.h"
 #include "linea.h"
 
-#include "math.h"
+#include <math.h>
 
 namespace colision
 {
-    colision_circulo_circulo(Circulo *prin, Circulo *secun)
+    PuntoDeColision colision_circulo_circulo(Circulo *prin, Circulo *secun)
     {
         Vector2 A = (secun->m_pos - prin->m_pos).normal() * prin->m_radio;
         Vector2 B = (prin->m_pos - secun->m_pos).normal() * secun->m_radio;
-        bool colisionan = pow(prin->m_radio + secun->m_radio, 2) >= (prin->m_pos - secun->m_pos).distancia_cuadrada());
+        bool colisionan = prin->m_radio + secun->m_radio >= (prin->m_pos - secun->m_pos).modulo();
 
-        return {A, B, (B - A).normal(), (B - A).distancia(), colisionan};
+        return {A, B, (B - A).normal(), (B - A).modulo(), colisionan};
     }
 
-    colision_circulo_linea(Circulo *circulo, Linea *linea)
+    PuntoDeColision colision_circulo_linea(Circulo *circulo, Linea *linea)
     {
+        return {};
+    }
+
+    PuntoDeColision colision_linea_linea(Linea *prin, Linea *secun)
+    {
+        return {};
     }
 }
