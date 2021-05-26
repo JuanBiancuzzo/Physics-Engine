99fe11b7e20cca9065cab5a03a2ec14246830891
JuanBiancuzzo <jbiancuzzo@fi.uba.ar>
2021-05-26 11:32:24 -0300

    empezando las pruebas de quadtree

--- tests/quadtree_test.cpp
+++ tests/quadtree_test.cpp
@@ -3,8 +3,8 @@
 
 TEST(QuadtreeTest, Entidad_en_rango)
 {
-    QuadTree qt(Vector2(.0f, .0f), 64.0f, 64.0f);
-    Circulo circulo(Vector2(.0f, .0f), 5.0f);
+    QuadTree qt(Vector2(), 64.0f, 64.0f);
+    Circulo circulo(Vector2(), 5.0f);
     Entidad entidad(&circulo);
 
     ASSERT_TRUE(qt.insertar(&entidad));
