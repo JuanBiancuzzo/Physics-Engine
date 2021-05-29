eec729d755dec87303e24a9bc9c076ee842edaf8
JuanBiancuzzo <jbiancuzzo@fi.uba.ar>
2021-05-29 18:04:42 -0300

    Haciendo test de los cuerpos para ver si funcionan bien

---
+++ tests/cuerpos_test.cpp
@@ -0,0 +1,20 @@
+#include "gtest/gtest.h"
+#include "../src/cuerpos/colisiones.h"
+
+TEST(CuerposTest, Colision_entre_circulo_y_aabb_en_rango)
+{
+    AABB rect(Vector2(), 10.0f, 10.0f);
+    Circulo circulo(Vector2(), 10.0f);
+    PuntoDeColision punto_de_colision = circulo.colisiona(&rect);
+
+    ASSERT_TRUE(punto_de_colision.colisiono);
+}
+
+TEST(CuerposTest, Colision_entre_circulo_y_aabb_fuera_de_rango)
+{
+    AABB rect(Vector2(), 10.0f, 10.0f);
+    Circulo circulo(Vector2(100.0f, .0f), 10.0f);
+    PuntoDeColision punto_de_colision = circulo.colisiona(&rect);
+
+    ASSERT_FALSE(punto_de_colision.colisiono);
+}
