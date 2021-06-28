d37e9df50463339953667a73bc884b67adc85fec
JuanBiancuzzo <jbiancuzzo@fi.uba.ar>
2021-06-27 22:23:53 -0300

    Haciendo una prueba con el pendulo de newton Parece que funciona bastante bien cuando la direccion en la que se mueve la velocidad no tiene que encontrarse con una particula estatica

--- tests/sistema_test.cpp
+++ tests/sistema_test.cpp
@@ -125,6 +125,55 @@ TEST(SistemaTest, Dos_particulas_sobre_el_piso_una_tienen_velocidades_y_hay_un_c
     ASSERT_EQ(particula2->m_fuerza, Vector2());
 }
 
+TEST(SistemaTest, Simular_el_pendulo_de_newton_con_todas_las_particulas_de_la_misma_masa)
+{
+    std::vector<Particula *> particulas;
+    Particula *particula1 = new Particula(1.0f, Vector2(10.0f, .0f), Vector2(.0f, -10.0f));
+    Particula *particula2 = new Particula(1.0f, Vector2(.0f, .0f), Vector2(.0f, -10.0f));
+    Particula *particula3 = new Particula(1.0f, Vector2(.0f, .0f), Vector2(.0f, -10.0f));
+    Particula *particula4 = new Particula(1.0f, Vector2(.0f, .0f), Vector2(.0f, -10.0f));
+    Particula *particula5 = new Particula(1.0f, Vector2(.0f, .0f), Vector2(.0f, -10.0f));
+    Particula *piso = new Particula();
+
+    particulas.emplace_back(particula1);
+    particulas.emplace_back(particula2);
+    particulas.emplace_back(particula3);
+    particulas.emplace_back(particula4);
+    particulas.emplace_back(particula5);
+    particulas.emplace_back(piso);
+
+    float dt = 1.0f;
+    Sistema sistema(particulas, dt);
+
+    Vector2 dir_abajo(.0f, -1.0f), dir_arriba(.0f, 1.0f), dir_derecha(1.0f, .0f), dir_izquierda(-1.0f, .0f);
+    sistema.agregar_interaccion(particula1, piso, dir_abajo);
+    sistema.agregar_interaccion(particula2, piso, dir_abajo);
+    sistema.agregar_interaccion(particula3, piso, dir_abajo);
+    sistema.agregar_interaccion(particula4, piso, dir_abajo);
+    sistema.agregar_interaccion(particula5, piso, dir_abajo);
+    sistema.agregar_interaccion(piso, particula1, dir_arriba);
+    sistema.agregar_interaccion(piso, particula2, dir_arriba);
+    sistema.agregar_interaccion(piso, particula3, dir_arriba);
+    sistema.agregar_interaccion(piso, particula4, dir_arriba);
+    sistema.agregar_interaccion(piso, particula5, dir_arriba);
+    sistema.agregar_interaccion(particula1, particula2, dir_derecha);
+    sistema.agregar_interaccion(particula2, particula1, dir_izquierda);
+    sistema.agregar_interaccion(particula2, particula3, dir_derecha);
+    sistema.agregar_interaccion(particula3, particula2, dir_izquierda);
+    sistema.agregar_interaccion(particula3, particula4, dir_derecha);
+    sistema.agregar_interaccion(particula4, particula3, dir_izquierda);
+    sistema.agregar_interaccion(particula4, particula5, dir_derecha);
+    sistema.agregar_interaccion(particula5, particula4, dir_izquierda);
+
+    sistema.expandir_fuerzas();
+
+    ASSERT_EQ(particula1->m_velocidad, Vector2(.0f, .0f));
+    ASSERT_EQ(particula2->m_velocidad, Vector2(.0f, .0f));
+    ASSERT_EQ(particula3->m_velocidad, Vector2(.0f, .0f));
+    ASSERT_EQ(particula4->m_velocidad, Vector2(.0f, .0f));
+    ASSERT_EQ(particula5->m_velocidad, Vector2(10.0f, .0f));
+}
+
 // test donde hay dos particulas cayendose, a la misma velocidad por lo que no deberia haber un choque -> a determinar
 
 // test donde hay dos particulas cayendose, la de abajo con una mayor velocidad por lo que no deberia haber un choque -> a determinar
