061734c37a003043451fa171f63a2ba421985605
JuanBiancuzzo <jbiancuzzo@fi.uba.ar>
2021-07-16 12:26:25 -0300

    Combinando los archivos para que sea mas practico

--- src/vector.cpp
+++ src/vector.cpp
@@ -1,6 +1,108 @@
-#include "vector3.h"
+#include "vector.h"
 
 #include <math.h>
+
+Vector2::Vector2()
+    : x(.0f), y(.0f)
+{
+}
+
+Vector2::Vector2(float x, float y)
+    : x(x), y(y)
+{
+}
+
+Vector2 Vector2::operator+(const Vector2 &otro) const
+{
+    return Vector2(x + otro.x, y + otro.y);
+}
+
+Vector2 Vector2::operator-(const Vector2 &otro) const
+{
+    return Vector2(x - otro.x, y - otro.y);
+}
+
+Vector2 Vector2::operator*(float alpha) const
+{
+    return Vector2(x * alpha, y * alpha);
+}
+
+float Vector2::operator*(const Vector2 &otro) const
+{
+    return (x * otro.x + y * otro.y);
+}
+
+Vector2 Vector2::operator/(float alpha) const
+{
+    return Vector2(x / alpha, y / alpha);
+}
+
+void Vector2::operator+=(const Vector2 &otro)
+{
+    x += otro.x;
+    y += otro.y;
+}
+
+void Vector2::operator-=(const Vector2 &otro)
+{
+    x -= otro.x;
+    y -= otro.y;
+}
+
+void Vector2::operator*=(float alpha)
+{
+    *this = *this * alpha;
+}
+
+void Vector2::operator/=(float alpha)
+{
+    *this = *this / alpha;
+}
+
+bool Vector2::operator==(const Vector2 &otro) const
+{
+    bool en_x = (this->x - delta < otro.x and otro.x < this->x + delta);
+    bool en_y = (this->y - delta < otro.y and otro.y < this->y + delta);
+
+    return en_x && en_y;
+}
+
+float Vector2::modulo() const
+{
+    return sqrt(pow(x, 2) + pow(y, 2));
+}
+
+float Vector2::modulo_cuadrado() const
+{
+    return pow(x, 2) + pow(y, 2);
+}
+
+float Vector2::distancia(const Vector2 &otro) const
+{
+    return (*this - otro).modulo();
+}
+
+float Vector2::distancia_cuadrada(const Vector2 &otro) const
+{
+    return (*this - otro).modulo_cuadrado();
+}
+
+Vector2 Vector2::proyeccion(const Vector2 &otro) const
+{
+    return otro * (((*this) * otro) / otro.modulo_cuadrado());
+}
+
+Vector2 Vector2::normal() const
+{
+    if (this->modulo() == 0.0f)
+        return *this;
+    return (*this / this->modulo());
+}
+
+bool Vector2::nulo() const
+{
+    return (*this == *this * .0f);
+}
 
 Vector3::Vector3()
     : x(.0f), y(.0f), z(.0f)
