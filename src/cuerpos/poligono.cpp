b80c071c406b991b3b1720f7a84aa1e094241831
JuanBiancuzzo <jbiancuzzo@fi.uba.ar>
2021-07-17 11:19:08 -0300

    Agregando el constructor de la clase poligono

--- src/cuerpos/poligono.cpp
+++ src/cuerpos/poligono.cpp
@@ -3,19 +3,19 @@
 using namespace cr;
 
 template <int cant_vertices>
-Vector2 Poligono<cant_vertices>::calcular_centro()
+Vector2 calcular_centro(std::array<Vector2, cant_vertices> vertices)
 {
     Vector2 centro;
-    for (Vector2 vertice : m_vertices)
+    for (Vector2 vertice : vertices)
         centro += vertice;
     return centro / cant_vertices;
 }
 
-// template <int cant_vertices>
-// Poligono<cant_vertices>::Poligono(std::array<Vector2, cant_vertices> vertices)
-//     : CuerpoRigido(calcular_centro<cant_vertices>(vertices)), m_vertices(vertices)
-// {
-// }
+template <int cant_vertices>
+Poligono<cant_vertices>::Poligono(std::array<Vector2, cant_vertices> vertices)
+    : CuerpoRigido(calcular_centro<cant_vertices>(vertices)), m_vertices(vertices)
+{
+}
 
 template <int cant_vertices>
 Vector2 Poligono<cant_vertices>::punto_soporte(Vector2 dir)
--- src/cuerpos/poligono.h
+++ src/cuerpos/poligono.h
@@ -12,10 +12,8 @@ namespace cr
         std::array<Vector2, cant_vertices> m_vertices;
 
     public:
-        // Poligono<cant_vertices>(std::array<Vector2, cant_vertices> vertices);
-        // virtual CuerpoRigido(Vector2 posicion) = 0;
+        Poligono<cant_vertices>(std::array<Vector2, cant_vertices> vertices);
 
         Vector2 punto_soporte(Vector2 dir);
-        Vector2 calcular_centro();
     };
 }
